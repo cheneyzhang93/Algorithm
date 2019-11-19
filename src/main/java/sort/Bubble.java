fb4b2de8863909c8f489ba036fa7c53963c6bb1a
Cheney <yuchen.zhang@bizatmobile.com>
2019-11-19 17:15:36 +0800

    冒泡排序

---
+++ src/main/java/sort/Bubble.java
@@ -0,0 +1,27 @@
+package sort;
+
+public class Bubble {
+
+    public static void main(String[] args) {
+
+        int[] arr = new int[]{3, 2, 5, 6, 8, 9, 1, 7, 4};
+        Bubble.bubbleSort(arr);
+        Common.print(arr);
+
+    }
+
+    public static void bubbleSort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+
+
+}
---
+++ src/main/java/sort/Common.java
@@ -0,0 +1,10 @@
+package sort;
+
+public class Common {
+
+    public static void print(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]);
+        }
+    }
+}
