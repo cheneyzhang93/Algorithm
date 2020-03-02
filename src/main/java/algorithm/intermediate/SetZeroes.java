5d8d3d9e652c6c6b6728fff679198b2e8980eec2
zhangyuchen <13155192291@163.com>
2020-03-02 22:18:21 +0800

    矩阵置零

---
+++ src/main/java/algorithm/intermediate/SetZeroes.java
@@ -0,0 +1,46 @@
+package algorithm.intermediate;
+
+/**
+ * https://leetcode-cn.com/explore/interview/card/top-interview-questions-medium/29/array-and-strings/76/
+ */
+public class SetZeroes {
+
+    public static void main(String[] args) {
+        int[][] matrix = new int[][]{
+                {1, 1, 1},
+                {1, 0, 1},
+                {1, 1, 1}
+        };
+        setZeroes(matrix);
+    }
+
+    public static void setZeroes(int[][] matrix) {
+        if (matrix.length == 0) return;
+
+        int[] rows = new int[matrix.length];
+        int[] cols = new int[matrix[0].length];
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (matrix[i][j] == 0) {
+                    rows[i] = 1;
+                    cols[j] = 1;
+                }
+            }
+        }
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (rows[i] == 1 || cols[j] == 1) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                System.out.print(matrix[i][j] + ",");
+            }
+            System.out.println();
+        }
+    }
+}
